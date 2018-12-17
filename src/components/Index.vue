f078f1bb5519c4347f1f1ed4a31cca6a8a969ff2
Fei Gao <fgao1@ualberta.ca>
2018-12-17 11:00:35 -0700

    get categories from all workouts, render on home page

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,12 +1,41 @@
 <template>
-  <div class="index container"></div>
+  <div class="index container center">
+    <ul class="categories">
+      <li v-for="(category,index) in categories" :key="index">
+        <a class="chip waves-effect waves-light btn">{{category}}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import db from "@/firebase/init";
+
 export default {
   name: "Index",
   data() {
-    return {};
+    return {
+      categories: []
+    };
+  },
+  methods: {
+    getCategories() {
+      let tempCat = [];
+      db.collection("workouts")
+        .get()
+        .then(workouts => {
+          workouts.forEach(workout => {
+            let singleCat = workout.data().category;
+            if (tempCat.indexOf("singleCat") == -1) {
+              tempCat.push(singleCat);
+            }
+          });
+          this.categories = tempCat;
+        });
+    }
+  },
+  created() {
+    this.getCategories();
   }
 };
 </script>
