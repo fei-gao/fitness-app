5b667b0b6d8caeac16afbe7739dbc020837320f7
Fei Gao <fgao1@ualberta.ca>
2018-12-18 11:23:17 -0700

    fix the error heatmap always shift one day ahead

--- src/components/Heatmap.vue
+++ src/components/Heatmap.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="center">
     <h3>Fitness Calendar</h3>
-<calendar-heatmap :values="options.values" :end-date="options.endDate" :range-color="options.rangeColor" :max="options.max" :vertical="options.vertical" :tooltip-unit="options.unit"/>
+    <calendar-heatmap
+      :values="options.values"
+      :end-date="options.endDate"
+      :range-color="options.rangeColor"
+      :max="options.max"
+      :vertical="options.vertical"
+      :tooltip-unit="options.unit"
+    />
   </div>
 </template>
 
 <script>
-import moment from "moment"
+import moment from "moment";
 import db from "@/firebase/init";
 
 export default {
-  name:'Heatmap',
+  name: "Heatmap",
   data() {
     return {
       options: {
         values: [],
-        endDate: '2018-12-31',
+        endDate: "2018-12-31",
         rangeColor: [
           "#eceff1",
           "#ccff90",
@@ -26,7 +33,7 @@ export default {
         ],
         max: 60,
         vertical: false,
-        unit: 'minutes'
+        unit: "minutes"
       }
     };
   },
@@ -39,13 +46,16 @@ export default {
         .then(workouts => {
           workouts.forEach(workout => {
             let singleWorkout = {};
-            singleWorkout.date = moment(workout.data().timestamp).format().slice(0, 10);
+            // here we get right date from timestamp, but I think the heatmap always shifted one day ahead, to color the correct date we delay one day on local variable, however the timestamp on firestore is corrected and untouched.
+            singleWorkout.date = moment(workout.data().timestamp + 24 * 3600000)
+              .format()
+              .slice(0, 10);
             singleWorkout.count = workout.data().duration;
             tempValues.push(singleWorkout);
-            })
+          });
           this.options.values = tempValues;
-          })
-        }
+        });
+    }
   },
   created() {
     this.getValues();
